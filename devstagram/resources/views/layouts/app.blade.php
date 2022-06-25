f11335e61374e82b2308f65b2f944da87e72fc81
Paulo <player_one_117@outlook.com>
2022-06-25 15:22:44 -0500

    Cierre de sesión seguro y menú adaptativo dependiendo de la sesión

--- devstagram/resources/views/layouts/app.blade.php
+++ devstagram/resources/views/layouts/app.blade.php
@@ -13,12 +13,28 @@
 <body class="bg-gray-100">
     <header class="p-5 border-b bg-white shadow">
         <div class="container mx-auto flex justify-between items-center">
-            <a href="{{ route('login') }}"><h1 class="text-3xl font-black">DevStagram</h1></a>
+            <a href="{{ route('login') }}">
+                <h1 class="text-3xl font-black">DevStagram</h1>
+            </a>
             {{--------------------------------- Menú ------------------------------------------------}}
+            @auth
+            <nav class="flex gap-2 items-center">
+                <a href="#" class="font-bold text-gray-600 text-sm">
+                    Hola <span class="font-normal">{{auth()->user()->username}}</span>
+                </a>
+                <form action="{{ route('logout')}}" method="POST">
+                    @csrf
+                    <button type="submit" class="font-bold uppercase text-gray-600 text-sm">Cerrar sesión</button>
+                </form>
+            </nav>
+            @endauth
+
+            @guest
             <nav class="flex gap-2 items-center">
                 <a href="{{ route('login') }}" class="font-bold uppercase text-gray-600 text-sm">Login</a>
                 <a href="{{ route('register') }}" class="font-bold uppercase text-gray-600 text-sm">Crear cuenta</a>
             </nav>
+            @endguest
         </div>
     </header>
 
