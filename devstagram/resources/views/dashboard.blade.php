54c91b2bb1b491fa88aeb26e127157e3c4d7b67c
Paulo <player_one_117@outlook.com>
2022-06-25 18:55:03 -0500

    Ruta de post usuario dinamica , login enviando el username para la url de post. envio de la información del usuario a la vista post (dashboard) para su uso Vista dashboard dinamica que muestra la información del usuario actual

--- devstagram/resources/views/dashboard.blade.php
+++ devstagram/resources/views/dashboard.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 
-@section('titulo', 'Tu cuenta')
+@section('titulo')
+    {{$user->username}}
+@endsection
 
 @section('contenido')
 <div class="flex justify-center">
@@ -8,8 +10,19 @@
         <div class="md:w-8/12 lg:w-6/12 px-5">
             <img src="{{asset('img/usuario.svg')}}" alt="imagen usuario"></img>
         </div>
-        <div class="md:w-8/12 lg:w-6/12 px-5">
-            <p class="text-gray-700 text-2xl">{{auth()->user()->username}}</p>
+        <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col items-center md:justify-center md:items-start py-10">
+            <div class="items-start">
+                <p class="text-gray-700 text-2xl mb-5">{{$user->username}}</p>
+                <p class="text-gray-800 text-sm mb-3 font-bold">
+                    0 <span class="font-normal">Seguidores</span>
+                </p>
+                <p class="text-gray-800 text-sm mb-3 font-bold">
+                    0 <span class="font-normal">Siguiendo</span>
+                </p>
+                <p class="text-gray-800 text-sm mb-3 font-bold">
+                    0 <span class="font-normal">Posts</span>
+                </p>
+            </div>
         </div>
     </div>
 </div>
